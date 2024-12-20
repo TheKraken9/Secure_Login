c09f0ea1f4e15a48eb73fb2515d52ca7d95a3639
Andrianambinina Fenitra <andrianambininafenitra@gmail.com>
2024-12-20 15:53:25 +0300

    first commit

---
+++ src/test/java/org/project/secure_login/SecureLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.secure_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
